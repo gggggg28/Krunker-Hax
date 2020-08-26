c3ca692c6cf5c319e837f3675c343bdd45cf4a2c
chonker1337 <66527953+chonker1337@users.noreply.github.com>
2020-08-26 22:10:22 +0200

    Create main.user.js

---
+++ main.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Simple ESP
+// @author       chomler
+// @match        *://krunker.io/*
+// @grant        none
+// ==/UserScript==
+
+Function.prototype.toString = new Proxy(Function.prototype.toString, {
+    apply(target, thisArg, argArray) {
+        let ret = target.apply(thisArg, argArray);
+        if (ret.length > 3500000 && ret.includes("function anonymous"))
+            Object.defineProperty(Object.prototype, /if\(!\w+\['(\w+)']\)continue/.exec(ret)[1], {value: true, enumerable: false});
+        return ret
+    }
+})
